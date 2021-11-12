389b2605b4a0df2609e8d62fa69d549bdef7a449
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-11-12 22:23:11 +0100

    simbad improve

--- simbad.py
+++ simbad.py
@@ -227,7 +227,11 @@ Simbad.add_votable_fields("coo_wavelength")
 # http://simbad.u-strasbg.fr/simbad/otypes#HXB
 
 ivoa_ot_G = rdflib.Graph()
-ivoa_ot_G.parse('https://www.ivoa.net/rdf/object-type/2020-10-06/object-type.rdf', format='turtle')
+
+# why does it not work by url?
+ivoa_ot_G.parse('https://www.ivoa.net/rdf/object-type/2020-10-06/object-type.rdf', format='xml')
+#ivoa_ot_G.parse(requests.get('https://www.ivoa.net/rdf/object-type/2020-10-06/object-type.rdf').text, format='xml')
+
 
 for w in odakb.sparql.select('?aobj a oda:AstrophysicalObject; rdfs:label ?label'): 
     # r = f'''
@@ -237,22 +241,50 @@ for w in odakb.sparql.select('?aobj a oda:AstrophysicalObject; rdfs:label ?label
 
     # print(r)
 
+    if 'EXO' not in w['label']:
+        continue
+    
     result_table = Simbad.query_object(w['label'], wildcard=False)
     print(result_table)
+
+    if result_table is None:
+        continue
 
     otype = str(result_table[0]['OTYPE']).strip()
 
     print(f"\"{otype}\"")
 
+    # TODO: deeper
+
+    G = rdflib.Graph()
+    for t in ivoa_ot_G.query(f'CONSTRUCT WHERE {{ ?obj rdfs:label "{otype}"; ?p ?o }}'):
+        G.add(t)
+
+    print(G.serialize(format='turtle'))
+
+    objs = list(ivoa_ot_G.query(f'SELECT ?obj WHERE {{ ?obj rdfs:label "{otype}" }}'))
     
     r = f'''
             <{w['aobj']}> oda:simbadOTYPE "{otype}" .
         '''
 
+    for obj in objs:
+        r += f'''
+            <{w['aobj']}> a <{obj[0]}> .
+        '''
+
+    for s, p, o in G:
+        r += f'''
+            {s.n3()} {p.n3()} {o.n3()} .
+        '''
+
+
+
     print(r)
 
     odakb.sparql.insert(r)
+    
 
-    break
+    #break
 
 
